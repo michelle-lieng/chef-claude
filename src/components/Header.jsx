55c6a7658d4a2993a85ae0ab06dd054f12fc6f30
Michelle Lieng <michellelieng2020@gmail.com>
2025-05-03 21:29:07 +1000

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="Header">
+            <img src="src\assets\chef-claude-icon.png" alt="Chef claude icon" />
+            <h1>Chef Claude</h1>  
+        </div>
+    )
+}
+
+export default Header
