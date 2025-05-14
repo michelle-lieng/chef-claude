b148d18d927a3c3a1e9a1b18e19d881bab2bc105
Michelle Lieng <michellelieng2020@gmail.com>
2025-05-15 00:23:25 +1000

    initial UI layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <div className="Header">
             <img src="src\assets\chef-claude-icon.png" alt="Chef claude icon" />
-            <h1>Chef Claude</h1>  
+            <h2>Chef Claude</h2>  
         </div>
     )
 }
